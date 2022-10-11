adc67529a8c0e78afe0e6681112261af65413e18
Candida <candidaratrisadew@gmail.com>
2022-10-11 22:20:27 +0700

    maaf jika salah

--- soal-string.php
+++ soal-string.php
@@ -34,8 +34,14 @@
         echo "<h3> Soal No 1</h3>";
 
         $first_sentence = "Hello PHP!" ; // Panjang string 10, jumlah kata: 2
+        echo "Panjang string: ". strlen($first_sentence);
+        echo " Jumlah kata: ". str_word_count($first_sentence);
+        echo " Dari kata Hello PHP!";
+        echo "<br>";
         $second_sentence = "I'm ready for the challenges"; // Panjang string: 28,  jumlah kata: 5
-        
+        echo "Panjang string: ". strlen($second_sentence);
+        echo " Jumlah kata: ". str_word_count($second_sentence);
+        echo " Dari kata I'm ready for the challenges";
         echo "<h3> Soal No 2</h3>";
         /* 
             SOAL NO 2
@@ -48,15 +54,17 @@
         echo "<label>String: </label> \"$string2\" <br>";
         echo "Kata pertama: " . substr($string2, 0, 1) . "<br>" ; 
         // Lanjutkan di bawah ini
-        echo "Kata kedua: " ;
-        echo "<br> Kata Ketiga: " ;
+        echo "Kata kedua: ". substr($string2, 2, 5) ;
+        echo "<br> Kata Ketiga: ". substr($string2, 7, 10) ;
         echo "<h3> Soal No 3 </h3>";
         /*
             SOAL NO 3
             Mengubah karakter atau kata yang ada di dalam sebuah string.
         */
-        $string3 = "PHP is old but Good!";
-        echo "String: \"$string3\" "; 
+        echo $string3 = " String: PHP is old but Good!";
+        echo "<br>";
+        echo str_replace("Good", "awesome", "PHP is old but awesome");
+        
         // OUTPUT : "PHP is old but awesome"
 
     ?>
